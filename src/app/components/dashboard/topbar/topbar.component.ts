1bd7f4824e82d8e7407986627d5573b2162adcc7
dnielramos <137344333+dnielramos@users.noreply.github.com>
2025-11-26 08:04:47 -0500

    refactor: hide luna AI top button

--- src/app/components/dashboard/topbar/topbar.component.ts
+++ src/app/components/dashboard/topbar/topbar.component.ts
@@ -11,7 +11,8 @@ import {
   faSearch,
   faArrowRightFromBracket,
   faHome,
-  faMagic
+  faMagic,
+  faWandSparkles
 } from '@fortawesome/free-solid-svg-icons';
 import { TrmComponent } from "../../navbar/trm/trm.component";
 import { AuthService } from '../../../services/auth.service';
@@ -41,14 +42,14 @@ import { inject } from '@angular/core';
       <div class="flex items-center gap-2">
 
         <!-- AI Chat Button -->
-        <button (click)="chatService.toggle()" 
-                class="group relative p-2 rounded-xl bg-gray-900/5 hover:bg-gray-900/10 border border-purple-700 text-purple-800 transition-all duration-300 flex items-center gap-2 mr-2 overflow-hidden shadow-sm hover:shadow-md">
+        <!-- <button (click)="chatService.toggle()" 
+                class="group relative p-1 rounded-sm bg-gray-900/5 hover:bg-gray-900/10 border border-purple-700 text-purple-800 transition-all duration-300 flex items-center gap-2 overflow-hidden shadow-sm hover:shadow-md">
           <div class="absolute inset-0 bg-gradient-to-r from-purple-800/10 via-purple-700/10 to-orange-600/10 opacity-0 group-hover:opacity-100 transition-opacity"></div>
-          <fa-icon [icon]="faMagic" class="text-lg text-orange-600 animate-pulse"></fa-icon>
-          <span class="text-sm font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-700 via-purple-800 to-orange-600 hidden md:block">
+          <fa-icon [icon]="faWandSparkles" class="text-lg text-orange-600 animate-pulse"></fa-icon>
+          <span class="text-xs font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-700 via-purple-800 to-orange-600 hidden md:block">
             Luna AI
           </span>
-        </button>
+        </button> -->
 
         <!-- <button class="relative p-2 text-gray-600 hover:bg-gray-100 rounded focus:outline-none">
           <fa-icon [icon]="faBell" class="text-lg"></fa-icon>
@@ -82,6 +83,7 @@ export class TopbarComponent {
   faArrowRightFromBracket = faArrowRightFromBracket;
   faHome = faHome;
   faMagic = faMagic;
+  faWandSparkles = faWandSparkles;
 
   @Output() toggleSidebarMobile = new EventEmitter<void>();
   @Output() toggleSidebarDesktop = new EventEmitter<void>();

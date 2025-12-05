0f6d4873c57c4effc28be13cc45e12b38b7eebb9
dnielramos <137344333+dnielramos@users.noreply.github.com>
2025-12-05 04:12:39 -0500

    add: mapas y traducciones

--- src/app/components/inicio/dell-expertise/dell-expertise.component.ts
+++ src/app/components/inicio/dell-expertise/dell-expertise.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dell-expertise',
   standalone: true,
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './dell-expertise.component.html',
 })
 export class DellExpertiseComponent {}

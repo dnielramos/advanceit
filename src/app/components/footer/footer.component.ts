cac0112f29b4496e85f9cda1fa3ef54ea8d4080a
dnielramos <137344333+dnielramos@users.noreply.github.com>
2025-12-07 10:46:03 -0500

    styles: colores footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,9 +9,10 @@ import { TranslatePipe } from '@ngx-translate/core';
   template: `
     <section class="bg-linear-to-b from-purple-300 to-purple-900"></section>
     <footer
-      class="relative bg-linear-to-b from-gray-500 to-black text-white"
+      class="relative text-white"
     >
-      <div class="relative pt-10 pb-8 max-w-6xl mx-auto">
+
+      <div class="relative bg-advance-900 pt-10 max-w-6xl mx-auto">
         <hr class="my-8 border-gray-500" />
         <div
           class="grid grid-cols-1 justify-items-center text-lg md:grid-cols-5 gap-8"
@@ -179,9 +180,12 @@ import { TranslatePipe } from '@ngx-translate/core';
 
         <!-- ubicaciones y terminos  -->
 
-        <hr class="my-8 border-gray-500" />
-        <div
-          class="flex flex-col md:flex-row items-center justify-between text-sm text-gray-300"
+        <hr class="border-gray-500" />
+
+      </div>
+
+      <div
+          class="flex flex-col md:flex-row items-center p-4 bg-advance-950 justify-between text-sm text-gray-500"
         >
           <div
             class="flex flex-wrap items-center space-x-3 mb-4 md:mb-0 justify-center md:justify-start"
@@ -227,9 +231,9 @@ import { TranslatePipe } from '@ngx-translate/core';
               >{{ 'footer.termsAndPrivacy' | translate }}</a
             >
           </div>
-        </div>
       </div>
+
     </footer>
   `,
 })
-export class FooterComponent {}
+export class FooterComponent { }
